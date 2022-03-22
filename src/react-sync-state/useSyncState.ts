78a6a5542568de81e6023fdcf20b790c634e1c79
IshaBP <isha.pachkawade11@gmail.com>
2022-03-22 23:00:37 +0530

    useSyncState hook updated

--- src/react-sync-state/useSyncState.ts
+++ src/react-sync-state/useSyncState.ts
@@ -1,19 +1,19 @@
 import { useCallback, useRef, useState } from 'react';
 
-export const useSyncState = <T>(initialValue: T): [T, (value: T) => void] => {
-  const valueRef = useRef<T>(initialValue);
-  const [value, setValue] = useState<T>(initialValue);
+export const useSyncState = <T>(
+  initialValue: T,
+): [() => T, (value: T) => void] => {
+  const valueRef = useRef<T>(initialValue); // To store value, is a container which stores value, ref does not get updated when value changes
+  const [_, setValue] = useState<T>(initialValue); // To cause re-render
 
-  const setNewValue = (newValue: T) => {
-    valueRef.current = newValue;
-    setValue(newValue);
-  };
   return [
-    valueRef.current,
-    setNewValue,
-    // useCallback(
-    //  ,
-    //   [valueRef, setValue],
-    // ),
+    useCallback(() => valueRef.current, [valueRef]),
+    useCallback(
+      (newValue: T) => {
+        valueRef.current = newValue;
+        setValue(newValue);
+      },
+      [valueRef, setValue],
+    ),
   ];
 };
