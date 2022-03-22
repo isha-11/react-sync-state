ed22bed5272633c9ae716fb48a5c92e5d69732f7
IshaBP <isha.pachkawade11@gmail.com>
2022-03-22 23:00:24 +0530

    useSyncState hook in App TDD

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,13 +2,24 @@ import { render, screen } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import { App } from './App';
 
-describe('Input', () => {
-  it('should display new value on key down', () => {
-    render(<App />);
-    act(() => type('Jest is best'));
+describe('App', () => {
+  it.each`
+    length | input
+    ${1}   | ${'Small_value'}
+    ${5}   | ${'Long_value'}
+    ${10}  | ${'Camel_is_the_ship_of_the_desert'}
+    ${20}  | ${'Check_the_value'}
+  `(
+    'should display $length characters of $input correctly',
+    ({ length, input }: { length: number; input: string }) => {
+      render(<App length={length} />);
+      act(() => type(input));
 
-    expect(screen.getByLabelText('value')).toHaveTextContent('Jest is best');
-  });
+      expect(screen.getByLabelText('value')).toHaveTextContent(
+        input.slice(0, length),
+      );
+    },
+  );
 });
 
 const type = (word: string) => {
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useSyncState } from './react-sync-state/useSyncState';
-window.type = (element) => {
-  element.dispatchEvent(new KeyboardEvent('keydown', { key: 'a' }));
-  element.dispatchEvent(new KeyboardEvent('keydown', { key: 'b' }));
-  element.dispatchEvent(new KeyboardEvent('keydown', { key: 'c' }));
-  element.dispatchEvent(new KeyboardEvent('keydown', { key: 'd' }));
-  element.dispatchEvent(new KeyboardEvent('keydown', { key: 'e' }));
-  element.dispatchEvent(new KeyboardEvent('keydown', { key: 'f' }));
-  element.dispatchEvent(new KeyboardEvent('keydown', { key: 'g' }));
-};
 
-export const App = () => {
-  // const [value, setValue] = useState('');
-  const [value, setValue] = useSyncState('');
+export const App = ({ length }: { length: number }) => {
+  const [getValue, setValue] = useSyncState('');
 
   useEffect(() => {
     const onChange = (event: KeyboardEvent) => {
-      setValue(value + event.key);
-      console.log(value, event.key);
+      getValue().length < length && setValue(getValue() + event.key);
     };
 
     document.addEventListener('keydown', onChange);
 
-    return () => document.removeEventListener('keydown', onChange);
-  }, []);
+    return () => {
+      document.removeEventListener('keydown', onChange);
+    };
+  }, [getValue, setValue]);
 
   return (
     <>
       <main className='App' aria-label='value'>
-        {value}
+        {getValue()}
       </main>
     </>
   );
